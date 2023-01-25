8ecbd77ec60677dbf2f026378bb65f453eb751de
EkaterinaDJ <dejongekaterina@gmail.com>
2023-01-26 02:27:11 +0300

    TIK-001: add

---
+++ rectangle.py
@@ -0,0 +1,15 @@
+class Rectangle:
+    def __init__(self, a, b):
+        self.a =a
+        self.b =b
+    def get_area(self):
+        return self.a * self.b
+
+from rectangle import Rectangle
+
+rect_1 = Rectangle(3, 4)
+rect_2 = Rectangle(12, 5)
+
+print(rect_1.get_area())
+print(rect_2.get_area())
+
